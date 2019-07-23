5b0c75841003e867a01f7bc90165edaa2f5bd611
raetsth <thijs.raets@elision.eu>
2019-07-23 22:23:54 +0200

    config path to components order

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,70 @@
 module.exports = {
-  siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
-  },
-  plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sass",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/data`,
-        name: "pages",
-      },
+    siteMetadata: {
+        title: "Portfolio Thijs Raets",
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/img`,
-        name: "images",
-      },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [],
-      },
-    },
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: "./src/img/favicon.png",
-      },
-    },
-    "gatsby-plugin-netlify", // make sure to keep it last in the array
-  ],
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sass",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                path: `${__dirname}/src/data`,
+                name: "pages",
+            },
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                path: `${__dirname}/src/img`,
+                name: "images",
+            },
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-transformer-remark",
+            options: {
+                plugins: [],
+            },
+        },
+        {
+            resolve: "gatsby-plugin-netlify-cms",
+            options: {
+                modulePath: `${__dirname}/src/cms/cms.js`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+                logo: "./src/img/favicon.png",
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "UA-144246494-1",
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                //exclude: ["/preview/**", "/do-not-track/me/too/"],
+                // Delays sending pageview hits on route update (in milliseconds)
+                pageTransitionDelay: 0,
+                // Enables Google Optimize using your container Id
+                // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+                // Enables Google Optimize Experiment ID
+                // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+                // Set Variation ID. 0 for original 1,2,3....
+                // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+                // Any additional create only fields (optional)
+                sampleRate: 5,
+                siteSpeedSampleRate: 10,
+                cookieDomain: "https://friendly-yonath-3ad744.netlify.com",
+            },
+        },
+        "gatsby-plugin-netlify", // make sure to keep it last in the array
+    ],
 };
