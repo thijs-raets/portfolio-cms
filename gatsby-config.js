755cac45fc0c539439b88d528eec1f6b94b5e68d
raetsth <thijs.raets@elision.eu>
2019-07-24 18:05:49 +0200

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,28 +43,8 @@ module.exports = {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
                 trackingId: "UA-144246494-1",
-                // Defines where to place the tracking script - `true` in the head and `false` in the body
-                head: false,
-                // Setting this parameter is optional
-                anonymize: true,
-                // Setting this parameter is also optional
-                respectDNT: true,
-                // Avoids sending pageview hits from custom paths
-                //exclude: ["/preview/**", "/do-not-track/me/too/"],
-                // Delays sending pageview hits on route update (in milliseconds)
-                pageTransitionDelay: 0,
-                // Enables Google Optimize using your container Id
-                // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-                // Enables Google Optimize Experiment ID
-                // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-                // Set Variation ID. 0 for original 1,2,3....
-                // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-                // Any additional create only fields (optional)
-                sampleRate: 5,
-                siteSpeedSampleRate: 10,
-                cookieDomain: "https://friendly-yonath-3ad744.netlify.com",
             },
         },
-        "gatsby-plugin-netlify", // make sure to keep it last in the array
+            "gatsby-plugin-netlify", // make sure to keep it last in the array
     ],
 };
